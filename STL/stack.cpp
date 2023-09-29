4da4c73b694efe7c56847c5488e015b559ea1c41
Pranav Prakash Patki <91589434+iampran07@users.noreply.github.com>
2023-09-29 14:36:06 +0530

    Add files via upload

---
+++ STL/stack.cpp
@@ -0,0 +1,31 @@
+#include<pranav.h>
+using namespace std;
+
+int main()
+{
+    stack<int>s1;
+    //stack follow LIFO principle. 
+    //3 main operations are present:-
+    //1.push
+    //2.pop
+    //3.top
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    s1.push(40);
+    s1.push(50);
+   
+   //to print
+   while(!s1.empty()){
+    cout<<s1.top()<<endl;
+    s1.pop();
+   }
+
+   //to delete
+   s1.pop();
+
+   //top element
+   cout<<s1.top();
+
+   //same functions as that of vectors
+}
