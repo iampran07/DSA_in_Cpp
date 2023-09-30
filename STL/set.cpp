63e17e6c656b54bd95c2cf033e7f633c1c23fad4
Pranav Prakash Patki <91589434+iampran07@users.noreply.github.com>
2023-09-30 15:12:42 +0530

    Update set.cpp

--- STL/set.cpp
+++ STL/set.cpp
@@ -7,6 +7,7 @@ int main()
     //does not allow for data duplication
     //always stores data in sorted mannner.
     //elements are stored in a tree manner
+    //everything is in log(n)
     set<int>s;
     s.insert(2);
     s.insert(10);
@@ -21,4 +22,10 @@ int main()
     
     //find function returns the address of particular element
     auto it=s.find(2);
+
+    //Lower Bound --> gives address of first occurence of an element
+    auto it1=s.lower_bound(3);
+
+    //Upper Bound --> gives address of immediate next greater element of key
+    auto it2=s.upper_bound(5);
 }
