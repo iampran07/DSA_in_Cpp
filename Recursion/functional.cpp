d3534649798330340554c86543ddc1b338912d14
Pranav Prakash Patki <91589434+iampran07@users.noreply.github.com>
2023-10-01 12:33:38 +0530

    Create functional.cpp

---
+++ Recursion/functional.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+//function is recursively calling itself
+//will return value 
+
+int print(int n){
+    if(n<1)
+        return 0;
+    return n+print(n-1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<print(n);
+    return 0;
+}
