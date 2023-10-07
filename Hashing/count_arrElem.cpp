703e7bb3db1f093ca8d75e282ced027d3e939e75
Pranav Prakash Patki <91589434+iampran07@users.noreply.github.com>
2023-10-07 12:34:40 +0530

    Create count_arrElem.cpp

---
+++ Hashing/count_arrElem.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+//hasing is method of prefetching and storing
+//basic example to count number of elements in array.
+//if declare array inside main --> arr[10^6];
+//but if glovally --> arr[10^7];
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    //prefetching method
+    
+    int hash[13]={0}; //just beacuse we only have to show max 12 elements so 13 spaces                       required.
+    for(int i=0;i<n;i++){
+        hash[arr[i]] +=1;
+    }
+    
+    
+    //fetching method
+    
+    int q;
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        cout<<hash[num]<<endl;
+    }
+}
