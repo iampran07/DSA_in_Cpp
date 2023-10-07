d7e6651080d581a7e0a4f8bc0922a560a3e14085
Pranav Prakash Patki <91589434+iampran07@users.noreply.github.com>
2023-10-07 13:28:45 +0530

    Create map_count_arrElem.cpp

---
+++ Hashing/map_count_arrElem.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+
+//map stores value in sorted order and only those element/char are stored
+//which are present
+//maps are very beneficial in hashing.
+
+int main()
+{
+    int n;
+  cinn>>n;
+  for(int i=0;i<n;i++)
+    cin>>arr[i];
+    
+    map<int,int>mp;
+    for(int i=0;i<n;i++)
+        mp[arr[i]] +=1;
+    
+    int q;
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        cout<<map[num]<<endl;
+    }
+    return 0;
+}
